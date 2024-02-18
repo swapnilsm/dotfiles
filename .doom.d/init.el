16f14b09ce59d6882169b163f6ffdba967245606
swapnilsm <swapnilsm@gmail.com>
2024-02-19 00:02:20 +0530

    Added stanup updates and dictionary

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -99,7 +99,7 @@
        ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)       ; run code, run (also, repls)
        ;;gist              ; interacting with github gists
-       lookup                  ; navigate your code and its documentation
+       (lookup +dictionary)                  ; navigate your code and its documentation
        ;; (lsp +eglot)
        magit                   ; a git porcelain for Emacs
        ;;make              ; run make tasks from Emacs
